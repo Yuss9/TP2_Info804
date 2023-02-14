7f7535481cbf110fe6e5804865ba2c0626c7cb3e
Huseyin Yurtseven <yurtseven68210@hotmail.fr>
2023-02-14 10:46:36 +0100

    write TODO

--- WaterPlane.hpp
+++ WaterPlane.hpp
@@ -6,7 +6,7 @@ namespace rt
 
     struct WaterPlane : public GraphicalObject
     {
-    
+
         WaterPlane(Point3 c, Vector3 u, Vector3 v,
                    Material main_m, Material band_m, Real w) : c{c}, u{u}, v{v}, main_m{main_m}, band_m{band_m}, w{w} {};
 
@@ -44,7 +44,9 @@ namespace rt
 
         Vector3 getNormal(Point3 p)
         {
-            return p + u.cross(v);
+            // TODO
+            // return null vector
+            return Vector3(0, 0, 0);
         }
 
         Material getMaterial(Point3 p)
@@ -56,40 +58,8 @@ namespace rt
 
         Real rayIntersection(const Ray &ray, Point3 &p)
         {
-            Real t;
-            Vector3 N = this->getNormal(c) / this->getNormal(c).norm();
-            Vector3 W = ray.direction / ray.direction.norm();
-            Real wn = W.dot(N);
-            Vector3 tmp = (c - ray.origin) / (c - ray.origin).norm();
-            if (wn == 0.0f)
-            {
-
-                if ((N.dot(tmp) / wn) == 0)
-                {
-                    t = N.dot(tmp) / wn;
-                    p = ray.origin + W * t;
-                    if (t >= 0.0f)
-                    {
-                        // std::cout << p << std::endl;
-                        return -1.0f;
-                    }
-                }
-                else
-                {
-                    return 1.0f;
-                }
-            }
-            else
-            {
-                t = N.dot(tmp) / wn;
-                p = ray.origin + W * t;
-                if (t >= 0.0f)
-                {
-                    // std::cout << p << std::endl;
-                    return -1.0f;
-                }
-            }
-            return 1.0f;
+            // TODO
+            return 0;
         }
 
     public:
